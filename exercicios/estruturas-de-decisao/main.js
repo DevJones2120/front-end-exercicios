11f436f5ececa2f1312577c2e59803fdfbb93147
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-09-12 21:47:03 -0300

    Fazendo exercicios

---
+++ exercicios/estruturas-de-decisao/main.js
@@ -0,0 +1,37 @@
+alert("Bem vindo a página de aprovados do ENEM.")
+
+// Inserindo dados
+let dados = prompt("Insira seu nome")
+console.log(dados)
+
+    if ( typeof dados != "string" ) {
+        alert(" Insira seu nome por favor !")
+    } 
+
+
+// Verificação
+let  = prompt("Insira sua idade") 
+    console.log(idade)
+
+if (idade >10) {
+    console.log("Seja bem vindo " + dados + "!")
+} else {
+    alert("Infelizmente você não poderá prosseguir," + dados + ".")
+}
+
+
+// Comparadores
+let numero = prompt("Insira seu número de inscrição")
+
+if (numero >532 && numero <743) {
+    console.log("Parabéns ! " + dados + ", você está entre os convocados.")
+} else {
+    alert("Infelizmente você não está na lista. Bpoa sorte na próxima !")
+}
+
+// Consórcio
+
+let idade = 18
+let salário = 2
+
+let idade = prompt("Insira sua idade")
