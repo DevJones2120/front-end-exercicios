f53cf179f02d94b7401fad305e5a5de433d94e95
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-09-16 20:06:48 -0300

    tirando comentários

--- exercicios/estruturas-de-decisao/exercicios1/main.js
+++ exercicios/estruturas-de-decisao/exercicios1/main.js
@@ -1,61 +1,61 @@
-// alert("Bem vindo a página de aprovados do ENEM.")
+alert("Bem vindo a página de aprovados do ENEM.")
 
-// // Inserindo dados
-// let dados = prompt("Insira seu nome")
-//     console.log(dados)
+// Inserindo dados
+let dados = prompt("Insira seu nome")
+    console.log(dados)
 
-//     if ( typeof dados != "string" ) {
-//         alert(" Insira seu nome por favor !")
-//     } 
+    if ( typeof dados != "string" ) {
+        alert(" Insira seu nome por favor !")
+    } 
  
 
-// // 1 Verificação
-// let number = prompt("Insira um número") 
+// 1 Verificação
+let number = prompt("Insira um número") 
 
-//     if ( number > 10) {
-//         console.log("Seja bem vindo " + dados + "!")
-//     } else {
-//         alert("Infelizmente você não poderá prosseguir," + dados + ".")
-//     }
+    if ( number > 10) {
+        console.log("Seja bem vindo " + dados + "!")
+    } else {
+        alert("Infelizmente você não poderá prosseguir," + dados + ".")
+    }
 
-// // 2 Comparadores
-// let numero = prompt("Insira seu número de inscrição")
+// 2 Comparadores
+let numero = prompt("Insira seu número de inscrição")
 
-//     if ( numero > 532 && numero < 743 ) {
-//         console.log("Parabéns ! " + dados + ", você está entre os convocados.")
-//     } else {
-//         alert("Infelizmente você não está na lista. Bpoa sorte na próxima !")
-//     }
+    if ( numero > 532 && numero < 743 ) {
+        console.log("Parabéns ! " + dados + ", você está entre os convocados.")
+    } else {
+        alert("Infelizmente você não está na lista. Bpoa sorte na próxima !")
+    }
 
-// // 3 
-// let senha = prompt("Insira sua senha")
+// 3 
+let senha = prompt("Insira sua senha")
 
-//     if (senha == "Aluno") {
-//         console.log("Certo, vamos prosseguir !")
-//     } else {
-//         alert("Tente novamente mais tarde!")
-//     }
+    if (senha == "Aluno") {
+        console.log("Certo, vamos prosseguir !")
+    } else {
+        alert("Tente novamente mais tarde!")
+    }
 
 
-// 4 Consórcio
-// let idade 
-// let salario
+//4 Consórcio
+let idade 
+let salario
 
-// let = prompt("Insira sua idade")
-//     if (idade >= 18) {
-//         console.log("prossiga")
-//     } else {
-//         alert("Você não tem idade suficiente.")
-//     }
+let = prompt("Insira sua idade")
+    if (idade >= 18) {
+        console.log("prossiga")
+    } else {
+        alert("Você não tem idade suficiente.")
+    }
 
-// let = prompt("Quantos salários mínimos você recebe ?")
-//     if (salario >= 2) {
-//         console.log("você tem dois salários ou mais")
-//     } else (
-//         console.log("Sua renda não é suficiente.")
-//     )
+let = prompt("Quantos salários mínimos você recebe ?")
+    if (salario >= 2) {
+        console.log("você tem dois salários ou mais")
+    } else (
+        console.log("Sua renda não é suficiente.")
+    )
 
-// 5 Concurso
+//5 Concurso
 let experiencia = prompt("Insira sua experiência como professor");
 let pontuacao = 0;
 
@@ -79,9 +79,5 @@ let pontuacao = 0;
     }
 
     console.log("")
-
-
-
-
-   // 7
+    
    
