693e833fec5ff35d83cc3f9913bdb1bb29bd894d
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-09-16 21:49:30 -0300

    exercicios

---
+++ exercicios/estruturas-de-decisao/exercicios1/lacos-de-repeticao/main.js
@@ -0,0 +1,28 @@
+// prompt("Insira seu nome por favor")
+
+// // 1
+// let numero = 1;
+
+// while ( numero <= 2025) {
+//     console.log(numero);
+//     numero = numero + 1;
+// }
+
+
+// 2
+// let number = 90;
+
+// while ( number >= 77 ) {
+//     console.log(number);
+//     number = number - 1;
+// }
+
+// 3
+
+let n =  prompt("Insira um número");
+let base = 1;
+
+while ( base <= n) {
+    console.log(base);
+    base = base + 1
+}
