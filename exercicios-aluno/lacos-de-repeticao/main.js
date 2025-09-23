de91900950f2a94b7321ff84372bcaef63613d21
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-09-23 19:14:00 -0300

    exercíocios de laços de Repetição

--- exercicios-aluno/lacos-de-repeticao/main.js
+++ exercicios-aluno/lacos-de-repeticao/main.js
@@ -45,7 +45,7 @@ do {
 */
 
 
-// 1
+// 1 //
 // let number;
 
 // do {
@@ -53,7 +53,7 @@ do {
 // }   while (number != 1 && number != 0);
     
 
-// 2
+// 2 //
 // let senha = "Gato";
 // let senhaUsuario // Declaração
 // do {
@@ -69,7 +69,7 @@ do {
 
 
 
-//3
+// 3 //
 // let numero;
 // let numeroMaior = 0;
 // let contador = 0;
@@ -94,16 +94,95 @@ for (let contador = 0; contador <= 12; contador = contador + 2) {
 }
 */
 
-//1
+// 3 diferntes opções de incremento do valor de uma varíavel
+// cont++
+// cont = cont + 1
+// cont += 1
+
+// 1 //
 // for (let contador = 0; contador < 101; contador = contador +1) {
-//     console.log(contador)
+//     console.log(contador)    
 // }
 
 
-//2
-let peso = 0;
+// 2 //
+//let peso = 0;
+//let pesoTotal = 0;
+
+//for (let contador = 1; contador < 11; contador += 1) {
+    //peso = prompt("insira o peso da pessoa numero" + contador );
+    //pesoTotal = pesoTotal + Number(peso); //converter para número
+
+//}  
+
+//let media = pesoTotal / 10; //
+//console.log()
+
+//const quadrado = function (x) {
+//  return x * x;
+//};
+
+//const aviso = function (m) {
+//  m = ' AVISO > ' + m;
+//  console.log(m);
+//}
+
+//aviso('hora do intervalo!'); //argumento
+//aviso('')
+
+//invocação da função quadrado passando o argumento 12
+//console.log(quadrado(12));
+//console.log(quadrado(19));
+//console.log(quadrado(100));
+//console.log(quadrado(4));
+
+//let resultado = quadrado(12);
+// console.log(resultado);
 
 
-for (let contador = 1; contador < 10; contador++) {
-    peso += Number(prompt("insira seu peso"));
-}   alert('Média de pesos é:' +  (peso / 10));
+
+//const exibir = function(m) {
+//    console.log(m);
+//}
+//exibir('hello wolrd');
+//exibir('ola mundo');
+
+//const tipo = function(valor) {
+//  console.log(typeof valor);
+//}
+//tipo('isso é um string');
+//tipo(1);
+//tipo(true);
+
+/*const menorValor = function(a,b) {
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+console.log(menoreValor(4,4));
+console.log(menorValor(10,8));
+console.log(menorValor(100,90));*/
+
+function soma(a,b) {
+  return a + b;
+}
+
+function subtração(a,b) {
+  return a - b;
+}
+
+function divisão(a,b) {
+  return b !== 0 ? a / b : "erro de divisão por zero";
+}
+
+function multiplicação(a,b) {
+  return a * b;
+}
+
+console.log(soma(5, 3)); //8
+console.log(subtraçaõ(10, 4)); //9
+console.log(divisão(20, 5)); //4
+console.log(multiplicação(6, 7)); //42
