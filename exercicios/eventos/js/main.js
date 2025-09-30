47c2bffdd0d778d2892cd05a1c27b84ddce1c6d0
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-09-29 21:28:50 -0300

    movendo pasta eventos

---
+++ exercicios/eventos/js/main.js
@@ -0,0 +1,11 @@
+let adicaoItens = document.getElementById("novo-item")
+let nome = document.getElementById("nome-pessoa")
+
+
+/**
+ * Função para adicionar item
+ */
+function adicionarItem() {
+    let.value
+}
+
