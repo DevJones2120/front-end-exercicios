f2f06c37e5fe5ffa3fdc52ff6f2939fde78ede0d
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-10-01 21:02:52 -0300

    exercícios eventos

---
+++ exercicios-aluno/eventos/js/main.js
@@ -0,0 +1,27 @@
+const botao = document.getElementById("btn-adicionar");
+const item = document.getElementById("#novo-item");
+const pessoa = document.getElementById("#nome-pessoa");
+
+const listaDeItens = document.querySelector(".itens");
+
+
+function adicionaItem() {
+
+    if ( item) {
+
+
+    } else {
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+botao.onclick = adicionaItem
